1840584aaad570af99f2b63f64d3a6b737999fc2
Dmitrii Antonov <d.antonovbox@gmail.com>
2021-11-01 23:28:11 +0200

    The first page of the site. Markup and Styles

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,11 +1,26 @@
 window._ = require('lodash');
 
 /**
+ * We'll load jQuery and the Bootstrap jQuery plugin which provides support
+ * for JavaScript based Bootstrap features such as modals and tabs. This
+ * code may be modified to fit the specific needs of your application.
+ */
+
+/*//!Закомментируем данную конструкцию, т.к. здесь речь идет о jquery и popper.js. popper.js - это js библиотека в которой используется bootstrap для создания вслывающих окошек. Ну и сам bootstrap.. он подключит здесь bootstrap 4, он нам не нужен. Мы будем устанавливать bootstrap 5
+try {
+    window.Popper = require('popper.js').default;
+    window.$ = window.jQuery = require('jquery');
+
+    require('bootstrap');
+} catch (e) {}*/
+
+/**
  * We'll load the axios HTTP library which allows us to easily issue requests
  * to our Laravel back-end. This library automatically handles sending the
  * CSRF token as a header based on the value of the "XSRF" token cookie.
  */
 
+//Здесь подключается библиотека axios. С помощью нее мы будем делать Ajax запросы
 window.axios = require('axios');
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,4 @@
-const mix = require('laravel-mix');
+const mix = require('laravel-mix'); //подключается библиотека laravel-mix
 
 /*
  |--------------------------------------------------------------------------
@@ -6,12 +6,19 @@ const mix = require('laravel-mix');
  |--------------------------------------------------------------------------
  |
  | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel applications. By default, we are compiling the CSS
+ | for your Laravel application. By default, we are compiling the Sass
  | file for the application as well as bundling up all the JS files.
  |
  */
 
+/*mix.js('resources/js/app.js', 'public/js')
+    .vue()
+    .sass('resources/sass/app.scss', 'public/css');*/
+
 mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        //
-    ]);
+    .vue()
+    .sass('resources/sass/app.scss', 'public/css') //файл app.scss берется из данной папки, компилируется и копируется в папку public/css
+	.version(); //после добавления сюда в файл webpack.mix.js версионирования version(), если мы запустим работу скрипта сборщика (npm run dev), в папке public появится файл mix-manifest.json. В нем будут указаны уникальные идентификаторы для каждого файла стилей и скритов. Каждый раз, при сборке Вебпаком, данные файловые иденификаторы будут меняться, что не даст браузеру использовать файлы стилей и скриптов из кэша
+	//Елисеев: вершен (version() ), чтобы он добавлял хеши к этим вещам: app.css?id=33b5f339521e47175532
+
+mix.copy('resources/img', 'public/img'); //копируем файл img в папку public/img
